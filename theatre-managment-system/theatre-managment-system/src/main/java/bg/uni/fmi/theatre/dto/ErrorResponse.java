5df32ed358a20e00efe228ebe291f28fdcb3fa2d
SvetoslavDT <svetcvetkov@gmail.com>
2026-04-05 15:35:03 +0300

    Added some dependencies

---
+++ theatre-managment-system/theatre-managment-system/src/main/java/bg/uni/fmi/theatre/dto/ErrorResponse.java
@@ -0,0 +1,34 @@
+package bg.uni.fmi.theatre.dto;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+
+    private final LocalDateTime localDateTime = LocalDateTime.now();
+    private final int status;
+    private final String message;
+    private final String path;
+
+    public ErrorResponse(int status, String message, String path) {
+        this.status = status;
+        this.message = message;
+        this.path = path;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+}
